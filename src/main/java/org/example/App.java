2a0f449bb1f2f3f9f2467de2d671742bb1de700e
KatarzynaMartynaWojcik <katarzynamartyna.wojcik@gmail.com>
2023-04-20 20:13:58 +0200

    changes task3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,17 +6,25 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        String [] capitols = new String[]{"Barcelona","London","Athen"};
-        for(Integer i=0;i< capitols.length; i++){
-            String capitol=capitols[i];
-            System.out.println((i+1+" "+capitol));
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World!");
+        String[] capitols = new String[]{"Barcelona", "London", "Athen"};
+        for (Integer i = 0; i < capitols.length; i++) {
+            String capitol = capitols[i];
+            System.out.println((i + 1 + " " + capitol));
         }
 
         //Zadanie 3:        Stwórz tablicę liczb zmiennoprzecinkowych i następnie oblicz sumę oraz średnią tych liczb.
-        System.out.println( "Give me numbers:" );
-        Float [] numbers=new Float[3];
+
+        Float[] numbers = new Float[3];
+        numbers[0] = 0.5f;
+        numbers[1] = 0.7f;
+        numbers[2] = 0.9f;
+        numbers[3] = 0.1f;
+        System.out.println  (numbers);
+
+
     }
 }
