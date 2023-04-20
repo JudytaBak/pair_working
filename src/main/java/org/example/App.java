65d7efe54cab8ac691b77aac46dfc18193de90c6
JudytaBak <131193456+JudytaBak@users.noreply.github.com>
2023-04-20 19:52:47 +0200

    ex2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String [] capitols = new String[]{"Barcelona","London","Athen"};
+        for(Integer i=0;i< capitols.length; i++){
+            String capitol=capitols[i];
+            System.out.println((i+1+" "+capitol));
+        }
+
+        //Zadanie 3:        Stwórz tablicę liczb zmiennoprzecinkowych i następnie oblicz sumę oraz średnią tych liczb.
+        System.out.println( "Give me numbers:" );
+        Float [] numbers=new Float[3];
     }
 }
